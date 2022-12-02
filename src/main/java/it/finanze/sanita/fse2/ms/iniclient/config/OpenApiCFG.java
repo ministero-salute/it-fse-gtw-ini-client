2ed1d90a024c651dcbbd087c9cf3cff7b0286507
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2022-12-02 19:53:31 +0100

    fix ; Add swagger documentation and kafka log props

---
+++ src/main/java/it/finanze/sanita/fse2/ms/iniclient/config/OpenApiCFG.java
@@ -0,0 +1,108 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+package it.finanze.sanita.fse2.ms.iniclient.config;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.RequestBody;
+import io.swagger.v3.oas.models.servers.Server;
+
+/**
+ * OpenAPI configuration class
+ */
+@Configuration
+public class OpenApiCFG {
+
+    @Autowired
+	private CustomSwaggerCFG customOpenapi;
+
+    @Bean
+    public OpenApiCustomiser disableAdditionalRequestProperties() {
+
+        final List<String> required = new ArrayList<>();
+		required.add("file");
+        required.add("requestBody");
+
+        return openApi -> {
+
+            // Populating info section.
+            openApi.getInfo().setTitle(customOpenapi.getTitle());
+            openApi.getInfo().setVersion(customOpenapi.getVersion());
+            openApi.getInfo().setDescription(customOpenapi.getDescription());
+            openApi.getInfo().setTermsOfService(customOpenapi.getTermsOfService());
+
+            // Adding contact to info section
+            final Contact contact = new Contact();
+            contact.setName(customOpenapi.getContactName());
+            contact.setUrl(customOpenapi.getContactUrl());
+            contact.setEmail(customOpenapi.getContactMail());
+            openApi.getInfo().setContact(contact);
+
+            // Adding extensions
+            openApi.getInfo().addExtension("x-api-id", customOpenapi.getApiId());
+            openApi.getInfo().addExtension("x-summary", customOpenapi.getApiSummary());
+
+            for (final Server server : openApi.getServers()) {
+                final Pattern pattern = Pattern.compile("^https://.*");
+                if (!pattern.matcher(server.getUrl()).matches()) {
+                    server.addExtension("x-sandbox", true);
+                }
+            }
+
+            openApi.getComponents().getSchemas().values().forEach(schema -> {
+				schema.setAdditionalProperties(false);
+			});
+ 
+        };
+    }
+
+    /**
+     * Disable additional properties on every response object
+     * @return The {@link OpenApiCustomiser} instance
+     */
+    @Bean
+    public OpenApiCustomiser disableAdditionalResponseProperties() {
+        return openApi -> openApi.getComponents().
+            getSchemas().
+            values().
+            forEach( s -> s.setAdditionalProperties(false));
+    }
+
+    private Schema<?> getFileSchema(PathItem item) {
+		MediaType mediaType = getMultipartFile(item);
+		if (mediaType == null) return null;
+		return mediaType.getSchema();
+	}
+
+    private MediaType getMultipartFile(PathItem item) {
+		Operation operation = getOperation(item);
+		if (operation == null) return null;
+		RequestBody body = operation.getRequestBody();
+		if (body == null) return null;
+		Content content = body.getContent();
+		if (content == null) return null;
+        return content.get(org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE);
+	}
+
+    private Operation getOperation(PathItem item) {
+		if (item.getPost() != null) return item.getPost();
+		if (item.getPatch() != null) return item.getPatch();
+		if (item.getPut() != null) return item.getPut();
+		return null;
+	}
+}
