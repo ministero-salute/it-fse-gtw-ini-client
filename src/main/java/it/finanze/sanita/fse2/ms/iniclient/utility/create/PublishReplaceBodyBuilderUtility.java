8618e3974e164a35b9c5012e410b7a6f074734f3
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2024-04-05 16:59:29 +0200

    fix: ISSUE 723

--- src/main/java/it/finanze/sanita/fse2/ms/iniclient/utility/create/PublishReplaceBodyBuilderUtility.java
+++ src/main/java/it/finanze/sanita/fse2/ms/iniclient/utility/create/PublishReplaceBodyBuilderUtility.java
@@ -91,8 +91,6 @@ public final class PublishReplaceBodyBuilderUtility {
 		} else {
 			//Replace
 			List<SlotType1> associationObjectSlots = new ArrayList<>();
-			SlotType1 associationObjSlot = buildSlotObject("SubmissionSetStatus", null,Collections.singletonList("Original"));
-			associationObjectSlots.add(associationObjSlot);
 			associationObject = buildAssociationObject(
 					"urn:ihe:iti:2007:AssociationType:RPLC","SubmissionSet01_Association_1", URN_UUID + documentEntryUUID,uuid,associationObjectSlots);
 		}
