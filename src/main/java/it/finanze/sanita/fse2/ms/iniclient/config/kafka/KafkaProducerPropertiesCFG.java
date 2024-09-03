472a362c9cea09c88b3847caa92b4dd3431ec9b4
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2024-09-03 10:52:01 +0200

    feat: Remove unused classes

--- src/main/java/it/finanze/sanita/fse2/ms/iniclient/config/kafka/KafkaProducerPropertiesCFG.java
+++ src/main/java/it/finanze/sanita/fse2/ms/iniclient/config/kafka/KafkaProducerPropertiesCFG.java
@@ -1,40 +1,21 @@
-package it.finanze.sanita.fse2.ms.iniclient.config.kafka;
+// package it.finanze.sanita.fse2.ms.iniclient.config.kafka;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
 
-import lombok.Data;
+// import lombok.Data;
 
-/**
- *	Kafka producer properties configuration.
- */
-@Data
-@Component
-public class KafkaProducerPropertiesCFG {
-    
-	/**
-	 * Client id.
-	 */
-	@Value("${kafka.producer.client-id}")
-	private String clientId;
-	
-	/**
-	 * Retries.
-	 */
-	@Value("${kafka.producer.retries}")
-	private Integer retries;
-	
-	/**
-	 * Key serializer.
-	 */
-	@Value("${kafka.producer.key-serializer}")
-	private String keySerializer;
-	
-	/**
-	 * Value serializer.
-	 */
-	@Value("${kafka.producer.value-serializer}")
-	private String valueSerializer;
-	
-	 
-}
+// /**
+// * Kafka producer properties configuration.
+// */
+// @Data
+// @Component
+// public class KafkaProducerPropertiesCFG {
+
+// /**
+// * Client id.
+// */
+// @Value("${kafka.producer.client-id}")
+// private String clientId;
+
+// }
