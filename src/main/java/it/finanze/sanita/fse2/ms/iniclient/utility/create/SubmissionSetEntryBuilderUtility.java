7f2025e10b31f0e10c4bed26919b706f27fa655d
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2024-09-18 15:45:01 +0200

    fix: Issue 995

--- src/main/java/it/finanze/sanita/fse2/ms/iniclient/utility/create/SubmissionSetEntryBuilderUtility.java
+++ src/main/java/it/finanze/sanita/fse2/ms/iniclient/utility/create/SubmissionSetEntryBuilderUtility.java
@@ -145,6 +145,7 @@ public class SubmissionSetEntryBuilderUtility {
 
 		//Author
 		if(classificationAuthorType!=null){
+			classificationAuthorType.setClassifiedObject(SUBMISSION_ENTRY_ID);			
 			out.add(classificationAuthorType);
 		} else if(!StringUtility.isNullOrEmpty(author)) {
 		 	AuthorSlotDTO authorSlotDto = CommonUtility.buildAuthorSlot(authorRole, authorInstitution, author);
