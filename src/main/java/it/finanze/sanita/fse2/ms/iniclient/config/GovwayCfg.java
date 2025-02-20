8fe95302e020c38e574c2511f26da9bf6b1de704
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2025-02-20 16:46:11 +0100

    feat: Add proxy and basic auth

---
+++ src/main/java/it/finanze/sanita/fse2/ms/iniclient/config/GovwayCfg.java
@@ -0,0 +1,27 @@
+package it.finanze.sanita.fse2.ms.iniclient.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+ 
+
+@Configuration
+@Slf4j
+@Data
+public class GovwayCfg {
+
+	@Value("${govway-url}")
+	private String govwayUrl;
+
+	@Value("${govway-port}")
+	private int govwayPort;
+
+	@Value("${govway-user}")
+	private String govwayUser;
+
+	@Value("${govway-pass}")
+	private String govwayPass;
+
+}
